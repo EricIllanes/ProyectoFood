0fabfdb7891b025d56f3fb42591da6af4ecd648e
Eric Illanes <e.icuriqueo@gmail.com>
2022-02-03 02:13:53 -0300

    commit 03/02

--- client/src/components/detailsRecipe/detailRecipe.js
+++ client/src/components/detailsRecipe/detailRecipe.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import NavBar from "../navBar/navBar"
 
 
 export default function DetailRecipe() {
@@ -13,9 +14,10 @@ export default function DetailRecipe() {
     // }
 
     return (<div>
+        <NavBar />
         {
             detailsRecipe ?
-                <div>
+                <div className="detailcard">
                     <img src={detailsRecipe.image ? detailsRecipe.image :
                         "https://e7.pngegg.com/pngimages/614/789/png-clipart-round-black-spoon-and-fork-buffet-breakfast-food-cafe-restaurant-spoon-and-fork-food-breakfast-thumbnail.png"}
                         alt="img" />
@@ -26,8 +28,10 @@ export default function DetailRecipe() {
                     }</h3>
                     <h3>{detailsRecipe.spoonacularScore || detailsRecipe.score}</h3>
                     <h3>{detailsRecipe.healthScore || detailsRecipe.level}</h3>
-                    <h3> {detailsRecipe.resume || detailsRecipe.summary}</h3>
-                    <h3>{detailsRecipe.instructions || detailsRecipe.steps}</h3>
+                    <div className="resume" dangerouslySetInnerHTML={{ __html: detailsRecipe.summary || detailsRecipe.resume }}></div>
+                    {/* <h3 > {detailsRecipe.resume || detailsRecipe.summary}</h3> */}
+                    <div className="instruction" dangerouslySetInnerHTML={{ __html: detailsRecipe.instructions || detailsRecipe.steps }}></div>
+                    {/* <h3>{detailsRecipe.instructions || detailsRecipe.steps}</h3> */}
                 </div> :
                 <div>loading</div>
         }
